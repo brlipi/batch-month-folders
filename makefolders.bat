e81c2d5b473298d09ae3b326bbed638777d9e034
Breno Lipi <brenorlipi@hotmail.com>
2022-04-09 14:34:17 -0300

    Batch file added

---
+++ makefolders.bat
@@ -0,0 +1,26 @@
+@echo off
+setlocal EnableExtensions DisableDelayedExpansion
+:: chcp 65001 >nul Para exibir caracteres com acentos no cmd
+chcp 65001 >nul
+:: /a	Sets <string> to a numerical expression that is evaluated.
+set /a year=%date:~6,4% + 1
+if not exist "%year%" (
+	md "%year%"
+	md "%year%\01-Janeiro"
+	md "%year%\02-Fevereiro"
+	md "%year%\03-Março"
+	md "%year%\04-Abril"
+	md "%year%\05-Maio"
+	md "%year%\05-Junho"
+	md "%year%\06-Julho"
+	md "%year%\07-Agosto"
+	md "%year%\08-Setembro"
+	md "%year%\09-Outubro"
+	md "%year%\11-Novembro"
+	md "%year%\12-Dezembro"
+	echo "Pastas criadas"
+) else (
+	echo "A pasta %year% já existe"
+)
+echo year: %year%
+pause
